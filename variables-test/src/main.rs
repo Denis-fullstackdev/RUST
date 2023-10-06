6756518b818c3c88c970613d9154e990afdc3b6f
Denis-fullstackdev <107939962+Denis-fullstackdev@users.noreply.github.com>
2023-10-06 23:52:05 +0200

    add variables test

---
+++ variables-test/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let mut x = 35; // data type i32
+    let z: i64= 50; // data type i64
+    let y: u64 = 40; // data type u64 u=unsigned, only positive numbers
+    let f: f64 = 6.7; // floats
+    let b: bool = false; // boolean type
+
+    println!("Value variable is {}", x);
+    
+    x = 99;
+
+    println!("Value variable is {}", x);
+
+}
