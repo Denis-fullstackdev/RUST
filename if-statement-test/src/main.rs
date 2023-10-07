367f7b06193abe57d3ef7e93de7524bc791ee5a3
Denis-fullstackdev <107939962+Denis-fullstackdev@users.noreply.github.com>
2023-10-07 23:42:07 +0200

    add test if statements

---
+++ if-statement-test/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+    let n = 30;
+
+    if n < 30 {
+        println!("Value of n is less then 30!");
+    }
+
+    if n == 30 {
+        println!("Value of n is equal 30!");
+    }
+
+    if n > 30 {
+        println!("Value of n is greater then 30!");
+    }
+
+    if n != 30 {
+        println!("Value of n is NOT equal 30!");
+    } else {
+        println!("Value of n is equal 30! (Using else)");
+    }
+
+}
